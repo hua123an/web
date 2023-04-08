64d016674f4b8c7f215e2befd59ca3351ac96b24
hua123an <1994843581@qq.com>
2023-04-08 22:04:52 +0800

    JavaScript Commit

---
+++ JavaScript/AdvanceGrammer/Exception/ExceptionOverflow.js
@@ -0,0 +1,21 @@
+/*
+* Error对象提供有用属性：name 和 message
+* */
+/*
+* Error对象属性：
+* 属性                    描述
+* name                  设置或返回错误名
+* message               设置或返回错误信息(一条字符串)
+* */
+/*
+* Error Name Values
+* error的name属性可返回六个不同的值：
+* 错误名                   描述
+* EvalError               已在eval()函数中发生的错误
+* RangeError              已发生超出数字范围的错误
+* ReferenceError          已发生非法引用
+*
+*
+*
+*
+* */
